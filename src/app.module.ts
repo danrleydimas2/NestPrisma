38ecaf69b6a13bacd33053562fe652c2a4e5fb4d
danrleydimas <danrley-dimas@hotmail.com>
2024-11-14 15:25:59 -0300

    adding service controllers and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
