c066c57d0455b2e0f42b748d5d6d28d21f3c1bc7
danrleydimas <danrley-dimas@hotmail.com>
2024-12-06 15:41:56 -0300

    add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
@@ -9,6 +9,16 @@ import { DatabaseInterceptor } from './common/errors/interceptors/database.inter
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The Simple Blog API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,11 +26,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   // app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ConflictInterceptor());
   app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'Nome completo do usuario' })
   @IsString()
   @IsNotEmpty()
   name: string;
+  @ApiProperty({ description: 'Email do usuario' })
+  @IsNotEmpty()
   @IsEmail()
   email: string;
+  @ApiProperty({
+    description: 'Define se o usuario é admistrador',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
